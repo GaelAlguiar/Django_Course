cb6233a4f1f45c5b0226dc79b790952d0378b35b
GaelAlguiar <gaelalguiar@gmail.com>
2026-01-15 11:09:46 -0600

    Se agregan filtros a las variables

--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
 # Create your views here.
 
 def home(req):
+    today = date.today()
     return render(req,"landing/landing.html", {
-        "name": "Gael"
+        "name": "Gael",
+        "today": today,
+        "age":21
     })
